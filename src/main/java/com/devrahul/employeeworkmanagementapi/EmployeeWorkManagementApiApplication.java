9e260d5b50f4a4373ceff5c74708def61eaf6dde
Rahul Kumar <rahulcsofficial@gmail.comm>
2023-02-13 00:32:23 +0530

    first commit

---
+++ src/main/java/com/devrahul/employeeworkmanagementapi/EmployeeWorkManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.devrahul.employeeworkmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWorkManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWorkManagementApiApplication.class, args);
+	}
+
+}
